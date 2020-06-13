d8090ad2b59ad318a95f3f864b2ae35736adf329
vicky kumar pal <vickykumarpal56@gmail.com>
2020-06-13 02:05:11 -0700

    add

---
+++ Date05.java
@@ -0,0 +1,26 @@
+public class Date05
+{
+      public static void main(String args[])
+	  {
+	        int[] a=new int[2];
+			a[0]=13;
+			a[1]=14;
+			
+			System.out.println(a[1]);
+			
+			int[] b={13,42,42,42,};
+		    System.out.println(b[1]);
+			
+			for(int i=0;i<4;i++)
+			{
+			System.out.println(b[i]);
+   
+			}
+
+			
+			
+			
+				
+			
+	  }
+}
